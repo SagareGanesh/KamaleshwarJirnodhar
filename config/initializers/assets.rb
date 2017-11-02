45a4690bb739be0041eacb0f26650505c736a13c
Ganesh Sagare <ganesh@joshsoftware.com>
2017-11-03 00:41:42 +0530

    Integrated Theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( creative.min.css font-awesome.min magnific-popup.css bootstrap.min )
